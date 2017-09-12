9c158eaf1c7132e86025fb1cf9a1c5dc4ef46d19
qq076qq076 <qq076qq076@gmail.com>
2017-09-13 00:57:00 +0800

    動態表單

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  form: FormGroup;
+
+  constructor(private FormBuilder: FormBuilder) {
+
+  }
+
+  ngOnInit() {
+    this.form = this.FormBuilder.group({
+      formarray: this.FormBuilder.array([])
+    });
+  }
+
+  AddItom() {
+    let add, FormArray;
+    add = this.FormBuilder.group({ 'data': ['點此編輯'] });
+    FormArray = this.form.get('formarray') as FormArray;
+    FormArray.push(add);
+    console.log(FormArray);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterializeModule } from 'ng2-materialize';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterializeModule.forRoot(),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
